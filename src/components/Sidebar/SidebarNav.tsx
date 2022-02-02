927c97f1d91d340c8b652e1509683d2033875e34
Giovanni Bússola <you@example.com>
2022-02-02 09:15:53 -0300

    feat: add next links on buttons

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -12,12 +12,24 @@ export function SidebarNav() {
   return (
     <Stack spacing="12" align="flex-start">
       <NavSection title="GERAL">
-        <NavLink children="Dashboard" icon={RiDashboardLine} />
-        <NavLink children="Usuários" icon={RiContactsLine} />
+        <NavLink
+          children="Dashboard"
+          href="/dashboard"
+          icon={RiDashboardLine}
+        />
+        <NavLink children="Usuários" href="/users" icon={RiContactsLine} />
       </NavSection>
       <NavSection title="AUTOMAÇÃO">
-        <NavLink children="Formulários" icon={RiInputMethodLine} />
-        <NavLink children="Automação" icon={RiGitMergeLine} />
+        <NavLink
+          children="Formulários"
+          href="/forms"
+          icon={RiInputMethodLine}
+        />
+        <NavLink
+          children="Automação"
+          href="/automation"
+          icon={RiGitMergeLine}
+        />
       </NavSection>
     </Stack>
   );
